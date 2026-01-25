3894db6f5ed0841e5c8b6e1e9a313239a2a58bdb
Umang <umangsinghal2007@gmail.com>
2026-01-25 16:25:05 +0530

    styling

--- seeds/index.js
+++ seeds/index.js
@@ -21,13 +21,17 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image :`https://picsum.photos/1000?random=${Math.random()}`,
+            description: 'hlo my name is umang and this is a sample description for yelp camp',
+            price : Math.floor(Math.random() * 20) + 10
         })
         await camp.save();
     }
 }
 seedDB().then(() => {
     mongoose.connection.close();
+    console.log(`Database connection closed`);
 });
 
         
